043cd8de76d6d131a4cdc301e0ee09580cccdf60
take114514 <maibadilin@gmail.com>
2021-10-04 22:48:58 +0900

    auth.php 6-27行目新規作成

--- common/auth.php
+++ common/auth.php
@@ -3,3 +3,14 @@
 if(!isset($_SESSION)){
     session_start();
 }
+/**
+ * ログインしているかチェックする
+ * @return bool
+ */
+function isLogin() {
+    if (isset($_SESSION['user'])) {
+        return true;
+    }
+
+    return false;
+}
