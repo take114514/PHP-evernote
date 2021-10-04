d418f92fffd52d1a6971d05eeceb612ebc26e6d9
take114514 <maibadilin@gmail.com>
2021-10-04 23:06:17 +0900

    新規追加

--- common/auth.php
+++ common/auth.php
@@ -14,3 +14,14 @@ function isLogin() {
 
     return false;
 }
+/**
+ * ログインしているユーザーIDを取得する
+ * @return |null
+ */
+function getLoginUserId() {
+    if (isset($_SESSION['user'])) {
+        return $_SESSION['user']['id'];
+    }
+
+    return null;
+}
